8e207d5eb0af72f34bbd3c1503105a48f01bcb32
ArthurSantos19 <arthursantosprogrm@gmail.com>
2023-07-30 18:50:31 -0300

    Section de adoção com js

--- app.js
+++ app.js
@@ -1,10 +1,57 @@
+// Dados dos cachorros disponíveis para adoção
+const data = {
+  "cachorros": [
+    {
+      "nome": "Dobby",
+      "idade": "2 anos",
+      "raca": "Vira-lata",
+      "imagem": "caramelocapapreta.jpg"
+    },
 
-// document.addEventListener("DOMContentLoaded", function() {
-//     const carousel = new Flickity(".carousel", {
-//       cellAlign: 'right',
-//       contain: true,
-//     //   wrapAround: true,
-//       autoPlay: true
-//     });
-//   });
-  
+    {
+      "nome": "Dory",
+      "idade": "3 anos",
+      "raca": "Vira-lata",
+      "imagem": "pastor.jpg"
+    }
+  ]
+};
+
+const imagemadt = document.querySelector(".imagem-adt");
+
+function createCarousel(dogs) {
+  dogs.forEach((dog) => {
+    const dogContainer = document.createElement("div");
+    dogContainer.classList.add("dog-container");
+
+    const dogContainerInfo = document.createElement("div");
+    dogContainerInfo.classList.add("dog-container-info");
+
+    dogContainer.style.backgroundImage = `url(assets/${dog.imagem})`;
+
+    const paragraphName = document.createElement("p");
+    paragraphName.textContent = `Nome: ${dog.nome}`
+
+    const paragraphAge = document.createElement("p");
+    paragraphAge.textContent = `Idade: ${dog.idade}`;
+
+    dogContainerInfo.appendChild(paragraphName);
+    dogContainerInfo.appendChild(paragraphAge);
+
+    // Adicione dogContainerInfo como filho de dogContainer
+    dogContainer.appendChild(dogContainerInfo);
+
+    // Adicione dogContainer como filho de imagemadt
+    imagemadt.appendChild(dogContainer);
+  });
+
+  const flkty = new Flickity(imagemadt, {
+    contain: false,
+    prevNextButtons: false,
+    autoPlay: true,
+  });
+}
+
+// Chamando a função para criar o carrossel com base nos dados "data.cachorros"
+createCarousel(data.cachorros);
+
